e38f10ef38e77ff33f45c3d58ef28b7eaaca598c
WhiteBeltBoi <gergelypifko@gmail.com>
2026-04-02 12:56:57 +0200

    refactored send_text.py to have seperate functions.

--- send_text.py
+++ send_text.py
@@ -1,20 +1,23 @@
 import subprocess
 
-with open("log.txt", "r") as f:
-    info = f.read().split()
+def send_text():
+    with open("log.txt", "r") as f:
+        info = f.read().split()
 
-if info and info[-1] != "unavailable":
-    print("Great success!")
-    phone_number = "+36309507721"
-    message = "The baby jacket is available."
-    script = f'''
-    tell application "Messages"
-        set targetService to 1st service whose service type = iMessage
-        set targetBuddy to buddy "{phone_number}" of targetService
-        send "{message}" to targetBuddy
-    end tell
-    '''
+    if info and info[-1] != "unavailable":
+        print("Great success!")
+        phone_number = "+36309507721"
+        message = "The baby jacket is available."
+        script = f'''
+        tell application "Messages"
+            set targetService to 1st service whose service type = iMessage
+            set targetBuddy to buddy "{phone_number}" of targetService
+            send "{message}" to targetBuddy
+        end tell
+        '''
 
-    subprocess.run(["osascript", "-e", script], check=True)
-else:
-    exit(1)
+        subprocess.run(["osascript", "-e", script], check=True)
+    else:
+        exit(1)
+if __name__ == "__main__":
+    send_text()
