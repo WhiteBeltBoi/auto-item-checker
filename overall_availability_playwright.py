46ddde9c3324591add3814e808514c819a436b6f
WhiteBeltBoi <gergelypifko@gmail.com>
2026-04-05 11:23:56 +0200

    Added error logging and early exit if the playwright browser gets detected

--- overall_availability_playwright.py
+++ overall_availability_playwright.py
@@ -1,17 +1,28 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, TimeoutError as PTimeoutError
 from datetime import datetime
 
 def get_page_info(url):
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.firefox.launch(headless=False)
 
         page = browser.new_page()
         page.goto(url, timeout=10000)
-        size_dropdown = page.locator("div[role='combobox'][aria-labelledby='size-input-label']")
-        size_dropdown.click()
+        if page.locator("h1", has_text = "Access Denied").is_visible():
+            write_log("oo","Bot got detected")
+            exit()
+        try:
+            size_dropdown = page.locator("div[role='combobox'][aria-labelledby='size-input-label']")
+            size_dropdown.click(timeout = 10000)
+        except PTimeoutError:
+            write_log("oo","TimeoutError")
+            browser.close()
+        try:
+            size_options = page.locator("li[role='option'][data-value = '28']")
+            size_options.first.wait_for(state="visible", timeout=8000)
+        except PTimeoutError:
+            write_log("oo","TimeoutError")
+            browser.close()
 
-        size_options = page.locator("li[role='option'][data-value = '28']")
-        size_options.first.wait_for(state="visible", timeout=8000)
 
         my_size = size_options.text_content().split()
         browser.close()
@@ -20,8 +31,8 @@ def get_page_info(url):
 
         return size_availability
 
-def write_log(size_availability, type_):
-    width = 10
+def write_log(size_availability , type_):
+    width = 11
     date = datetime.now().strftime("%Y-%m-%d-%H-%M")
     log_file = "/Users/johndoe/PycharmProjects/baby_jacket_checker/web_scraping_baby_jacket/main.log"
     with open(log_file, "a") as logger:
